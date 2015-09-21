114c969c2f0bc25d609de21f52e4bc73e074737d
Kristen Spencer <kristen@kristenspencer.ca>
2015-09-20 22:11:37 -0400

    heading stylez

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href='https://fonts.googleapis.com/css?family=Dosis:400,600' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Dosis:400,500' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
 </head>
@@ -27,18 +27,23 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="main-nav grid-12">
-			<div class="site-branding grid-2 pad-2-left">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src=" <?php bloginfo(template_directory) ?>/img/mcclellandlogo.png " alt="McClelland Insurance Logo"></a></h1>
-			</div><!-- .site-branding -->
-			<nav id="site-navigation" class="main-navigation grid-6" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Services Menu', 'mcclellandinsurance' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'services', 'menu_id' => 'services' ) ); ?>
-			</nav><!-- #site-navigation -->
-			<form method="get" id="searchform" role="search" action="<?php bloginfo('home'); ?>/">
-				<div><input type="text" name="s" value="Search" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
-					<input type="submit" id="searchsubmit" value="Search" class="btn" />
-				</div>	
-			</form>
+			<div class="wrapper clearfix">
+				<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src=" <?php bloginfo(template_directory) ?>/img/mcclellandlogo.png " alt="McClelland Insurance Logo"></a></h1>
+				</div><!-- .site-branding -->
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Services Menu', 'mcclellandinsurance' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'services', 'menu_id' => 'services' ) ); ?>
+				</nav><!-- #site-navigation -->
+				<div class="search-container">
+					<form method="get" id="searchform" role="search" action="<?php bloginfo('home'); ?>/">
+						<div>
+							<label class="search_icon" for="s"></label>
+							<input type="text" name="s" value="Search..." onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
+						</div>	
+					</form>
+				</div>
+			</div>
 		</div><!-- /main-navigation -->
 		<div class="secondary-nav">
 			<nav id="secondary-nav" class="secondary-navigation" role="navigation">
