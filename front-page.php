d7450195f42ca71f4ee25f561fc8a3d93f1bb252
Kristen Spencer <kristen@kristenspencer.ca>
2015-09-21 23:12:48 -0400

    added slider to homepage

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="main-gallery">
+				<div class="gallery-cell"><img src="<?php bloginfo(template_directory) ?>/img/mcclellandinsurance_hero.png " alt=""></div>
+				<div class="gallery-cell"><img src=" <?php bloginfo(template_directory) ?>/img/filler-img.jpg" alt=""></div>
+			</div>
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
